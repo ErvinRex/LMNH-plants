083246ebe5bddc1c59c5f1ab1879765224559a95
RachelPun <rachelpunhokyan@ymail.com>
2024-04-16 18:07:55 +0100

    added stuff

---
+++ download_pymssql.sh
@@ -0,0 +1,5 @@
+brew install FreeTDS
+export CFLAGS="-I$(brew --prefix openssl)/include"
+export LDFLAGS="-L$(brew --prefix openssl)/lib -L/usr/local/opt/openssl/lib"
+export CPPFLAGS="-I$(brew --prefix openssl)/include"
+pip3 install --pre --no-binary :all: pymssql --no-cache
