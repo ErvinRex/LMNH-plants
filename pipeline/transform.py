dbd659f2db853e7b4f2a2d29cc09a6a419e7fa2c
dom-c1 <domgamin@gmail.com>
2024-04-16 10:31:12 +0100

    new

--- pipeline/transform.py
+++ pipeline/transform.py
@@ -9,35 +9,25 @@ def transform(data:list[dict]) -> dict:
     tables = {'images':[],'recording':[],'botanist':[],'plant':[],'origin':[]}
     for id_, data in enumerate(json):
         if data:
-            botanist_email = data.get('botanist', {}).get('email', 'No email')
-            botanist_name = data.get('botanist', {}).get('name', 'No name')
+            botanist_email = data.get('botanist', {}).get('email',None)
+            botanist_name = data.get('botanist', {}).get('name', None)
             first_name = botanist_name.split(' ')[0]
             second_name = botanist_name.split(' ')[1]
-            botanist_phone = data.get('botanist', {}).get('phone', 'No phone')
+            botanist_phone = data.get('botanist', {}).get('phone', None)
 
             
             images = data.get('images')
             if images:
-                license_name = images.get('license_name', 'No license name')
-                license_url = images.get('license_url', 'No license url')
-                medium_url = images.get('medium_url', 'No URL provided')
-                original_url = images.get('original_url', 'No URL provided')
-                regular_url = images.get('regular_url', 'No URL provided')
-                small_url = images.get('small_url', 'No URL provided')
-                thumbnail_url = images.get('thumbnail', 'No URL provided')
-                
-            else:
-                license_name = 'None'
-                medium_url ='None'  
-                original_url = 'None'
-                regular_url = 'None'
-                small_url = 'None'
-                thumbnail_url ='None'
+                lic = images.get('license', None)
+                license_name = images.get('license_name', None)
+                license_url = images.get('license_url', None)
+                original_url = images.get('original_url', None)
+                license_name = None
             
 
-            last_watered = data.get('last_watered', 'Not recorded')
-            plant_name = data.get('name', 'Unnamed plant')
-            origin_location = data.get('origin_location')
+            last_watered = data.get('last_watered', None)
+            plant_name = data.get('name', None)
+            origin_location = data.get('origin_location',None)
             if origin_location:
                 lat = origin_location[0]
                 lon = origin_location[1]
@@ -47,15 +37,18 @@ def transform(data:list[dict]) -> dict:
 
 
                 
-            plant_id = data.get('plant_id', -1)
-            recording_taken = data.get('recording_taken', 'No record')
-            scientific_name = data.get('scientific_name', ['Unknown'])
-            soil_moisture = data.get('soil_moisture', 0)
-            temperature = data.get('temperature', 0)
-            tables['plant'].append(([(plant_name,id_,scientific_name)]))
-            tables['image'].append((license_name,license_url,medium_url,original_url,regular_url,small_url, thumbnail_url))
-            tables['recording'.append(plant_id,recording_taken,last_watered,soil_moisture,temperature)]
+            plant_id = data.get('plant_id', None)
+            recording_taken = data.get('recording_taken', None)
+            scientific_name = data.get('scientific_name', None)
+            soil_moisture = data.get('soil_moisture', None)
+            temperature = data.get('temperature', None)
+            tables['plant'].append(((plant_name,id_,scientific_name)))
+            tables['images'].append((license_name,license_url,original_url,lic))
+            tables['recording'].append((plant_id,recording_taken,last_watered,soil_moisture,temperature))
             tables['origin'].append((lon,lat,place_name,country_code,timezone))
             tables['botanist'].append((botanist_email,botanist_phone,botanist_phone,first_name,second_name))
-
+            return tables
+json = get_test_file()
+transform(json)
+    
     
