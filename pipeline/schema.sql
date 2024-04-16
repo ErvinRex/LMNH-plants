709fadd9fe20f8857bc846a565c429e3904e63f2
ErvinRex <92224370+ErvinRex@users.noreply.github.com>
2024-04-16 12:35:48 +0100

    added database schema final

--- pipeline/schema.sql
+++ pipeline/schema.sql
@@ -1,53 +1,63 @@
 -- This file should contain all code required to create & seed database tables.
 
-CREATE TABLE IF NOT EXISTS botanist (
-    botanist_id SMALLINT UNIQUE GENERATED ALWAYS AS IDENTITY,
-    email VARCHAR(100) NOT NULL,
-    phone_number VARCHAR(30) NOT NULL,
-    first_name VARCHAR(20) NOT NULL,
-    last_name VARCHAR(20) NOT NULL,
-    PRIMARY KEY (botanist_id)
-);
+IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'botanist' AND schema_id = SCHEMA_ID('s_beta'))
+BEGIN
+    CREATE TABLE s_beta.botanist (
+        botanist_id SMALLINT IDENTITY(1, 1) PRIMARY KEY,
+        email VARCHAR(100) NOT NULL,
+        phone_number VARCHAR(30) NOT NULL,
+        first_name VARCHAR(20) NOT NULL,
+        last_name VARCHAR(20) NOT NULL
+    );
+END;
 
-CREATE TABLE plant (
-    plant_id SMALLINT UNIQUE NOT NULL,
-    plant_name VARCHAR(30) NOT NULL,
-    scientific_name VARCHAR(30),
-    origin_id SMALLINT UNIQUE NOT NULL,
-        FOREIGN KEY (origin_id) REFERENCES origin(origin_id),
-    PRIMARY KEY (plant_id)
-);
+IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'origin' AND schema_id = SCHEMA_ID('s_beta'))
+BEGIN
+    CREATE TABLE s_beta.origin (
+        origin_id SMALLINT IDENTITY(1, 1) PRIMARY KEY,
+        longitude DECIMAL(8, 2) NOT NULL,
+        latitude DECIMAL(8, 2) NOT NULL,
+        place_name VARCHAR(20) NOT NULL,
+        country_code VARCHAR(2) NOT NULL,
+        timezone VARCHAR(20) NOT NULL
+    );
+END;
 
-CREATE TABLE image (
-    image_id BIGINT GENERATED ALWAYS AS IDENTITY,
-    original_url TEXT,
-    license SMALLINT,
-    license_name VARCHAR(30),
-    license_url TEXT,
-    PRIMARY KEY (image_id)
-);
+IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'plant' AND schema_id = SCHEMA_ID('s_beta'))
+BEGIN
+    CREATE TABLE s_beta.plant (
+        plant_id SMALLINT PRIMARY KEY,
+        plant_name VARCHAR(30) NOT NULL,
+        scientific_name VARCHAR(30),
+        origin_id SMALLINT NOT NULL,
+            FOREIGN KEY (origin_id) REFERENCES s_beta.origin(origin_id)
+    );
+END;
 
-CREATE TABLE recording (
-    recording_id BIGINT GENERATED ALWAYS AS IDENTITY,
-    plant_id SMALLINT UNIQUE NOT NULL,
-        FOREIGN KEY (plant_id) REFERENCES plant(plant_id),
-    recording_taken TIMESTAMPTZ NOT NULL,
-    last_watered TIMESTAMPTZ,
-    soil_moisture DECIMAL(8, 4) NOT NULL,
-    temperature DECIMAL(8, 4) NOT NULL,
-    image_id BIGINT UNIQUE NOT NULL,
-        FOREIGN KEY (image_id) REFERENCES image(image_id),
-    botanist_id SMALLINT UNIQUE NOT NULL,
-        FOREIGN KEY (botanist_id) REFERENCES botanist(botanist_id),
-    PRIMARY KEY (recording_id)
-);
+IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'image' AND schema_id = SCHEMA_ID('s_beta'))
+BEGIN
+    CREATE TABLE s_beta.image (
+        image_id BIGINT IDENTITY(1, 1) PRIMARY KEY,
+        original_url NVARCHAR(MAX),
+        license SMALLINT,
+        license_name VARCHAR(30),
+        license_url NVARCHAR(MAX)
+    );
+END;
 
-CREATE TABLE origin (
-    origin_id SMALLINT GENERATED ALWAYS AS IDENTITY,
-    longitude DECIMAL(8, 2) NOT NULL,
-    latitude DECIMAL(8, 2) NOT NULL,
-    place_name VARCHAR(20) NOT NULL,
-    country_code VARCHAR(2) NOT NULL,
-    timezone VARCHAR(20) NOT NULL,
-    PRIMARY KEY (origin_id)
-);
+IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'recording' AND schema_id = SCHEMA_ID('s_beta'))
+BEGIN
+    CREATE TABLE s_beta.recording (
+        recording_id BIGINT IDENTITY(1, 1) PRIMARY KEY,
+        plant_id SMALLINT NOT NULL,
+            FOREIGN KEY (plant_id) REFERENCES s_beta.plant(plant_id),
+        recording_taken DATETIME2 NOT NULL,
+        last_watered DATETIME2,
+        soil_moisture DECIMAL(8, 4) NOT NULL,
+        temperature DECIMAL(8, 4) NOT NULL,
+        image_id BIGINT NOT NULL,
+            FOREIGN KEY (image_id) REFERENCES s_beta.image(image_id),
+        botanist_id SMALLINT NOT NULL,
+            FOREIGN KEY (botanist_id) REFERENCES s_beta.botanist(botanist_id)
+    );
+END;
