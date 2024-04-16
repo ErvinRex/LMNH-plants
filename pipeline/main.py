46d0d419a3f45e86a1c64d8b2885a017aa1f02a3
Adam <trainee.adam.osullivan@sigmalabs.co.uk>
2024-04-16 16:43:31 +0100

    added load functioanlity

--- pipeline/main.py
+++ pipeline/main.py
@@ -1,9 +1,12 @@
 import asyncio
 import time
+
 from dotenv import load_dotenv
 from extract import fetch_data_from_endpoints
 from transform import transform
+from load import upload_data
 from os import environ as ENV
+from pymssql import connect
 
 # Fetch AWS credentials from .env
 load_dotenv()
@@ -13,17 +16,26 @@ AWS_SECRET_ACCESS_KEY = ENV["AWS_SECRET_ACCESS_KEY"]
 # Config for uploading to AWS RDS
 DB_HOST = ENV["DB_HOST"]
 DB_NAME = ENV["DB_NAME"]
-DB_PORT = ENV["DB_PORT"]
 DB_USER = ENV["DB_USER"]
+DB_PORT = ENV["DB_PORT"]
 DB_PASSWORD = ENV["DB_PASSWORD"]
 DB_SCHEMA = ENV["DB_SCHEMA"]
 
 
 async def main():
+    conn = connect(
+        server=ENV["DB_HOST"],
+        user=ENV["DB_USER"],
+        password=ENV["DB_PASSWORD"],
+        database=ENV["DB_NAME"],
+        port=ENV["DB_PORT"],
+        as_dict=True,
+    )
+
     while True:
-        current_time = time.time()
-        seconds_until_next_minute = 60 - (current_time % 60)
-        await asyncio.sleep(seconds_until_next_minute)
+        # current_time = time.time()
+        # seconds_until_next_minute = 60 - (current_time % 60)
+        # await asyncio.sleep(seconds_until_next_minute)
 
         # start_time = time.time()
 
@@ -35,7 +47,7 @@ async def main():
 
         transform_data = transform(extract_data)
 
-        print(transform_data)
+        upload_data(transform_data, conn)
 
         # print(time.time() - start_time)
 
--- pipeline/transform.py
+++ pipeline/transform.py
@@ -1,9 +1,9 @@
-import json
+from datetime import datetime
 
 
 def transform(data: list[dict]) -> dict:
-
     tables = {"images": [], "recording": [], "botanist": [], "plant": [], "origin": []}
+
     for id_, data in enumerate(data):
         if data:
             botanist_email = data.get("botanist", {}).get("email", None)
@@ -15,12 +15,19 @@ def transform(data: list[dict]) -> dict:
             images = data.get("images")
             if images:
                 lic = images.get("license", None)
-                license_name = images.get("license_name", None)
+                license_name = images.get("license_name", None)[:29]
                 license_url = images.get("license_url", None)
                 original_url = images.get("original_url", None)
+            else:
+                lic = None
                 license_name = None
+                license_url = None
+                original_url = None
 
             last_watered = data.get("last_watered", None)
+            last_watered = datetime.strptime(
+                last_watered, "%a, %d %b %Y %H:%M:%S %Z"
+            ).strftime("%Y-%m-%d %H:%M:%S")
             plant_name = data.get("name", None)
             origin_location = data.get("origin_location", None)
             if origin_location:
@@ -33,9 +40,11 @@ def transform(data: list[dict]) -> dict:
             plant_id = data.get("plant_id", None)
             recording_taken = data.get("recording_taken", None)
             scientific_name = data.get("scientific_name", None)
+            if scientific_name:
+                scientific_name = scientific_name[0]
             soil_moisture = data.get("soil_moisture", None)
             temperature = data.get("temperature", None)
-            tables["plant"].append(((plant_name, id_, scientific_name)))
+            tables["plant"].append((plant_name, id_, scientific_name))
             tables["images"].append((license_name, license_url, original_url, lic))
             tables["recording"].append(
                 (plant_id, recording_taken, last_watered, soil_moisture, temperature)
@@ -44,7 +53,6 @@ def transform(data: list[dict]) -> dict:
             tables["botanist"].append(
                 (
                     botanist_email,
-                    botanist_phone,
                     botanist_phone,
                     first_name,
                     second_name,
