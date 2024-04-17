ed333b5e73089f67d8b9329a5144183ff514476e
RachelPun <rachelpunhokyan@ymail.com>
2024-04-17 17:13:47 +0100

    added st.metrics

--- dashboard/app.py
+++ dashboard/app.py
@@ -3,6 +3,7 @@ Python script for visualising and hosting a dashboard for LMNH plant data
 """
 
 from os import environ as ENV
+from datetime import datetime, timezone, timedelta
 from dotenv import load_dotenv
 import pandas as pd
 import streamlit as st
@@ -23,63 +24,79 @@ def get_db_connection(config: dict) -> connect:
     )
 
 
-# def get_merged_data(conn) -> pd.DataFrame:
-#     """Access redshift database to get merged data"""
-#     curr = get_cursor(conn)
-#     sql_query = """SELECT *
-#                     FROM s_beta.recording AS r
-#                     JOIN s_beta.plant AS p
-#                         ON r.plant_id = p.plant_id
-#                     JOIN s_beta.origin AS o
-#                         ON p.origin_id = o.origin_id
-#                     JOIN s_beta.botanist AS b
-#                         ON r.botanist_id = b.botanist_id
-#                     JOIN s_beta.image as i
-#                         ON r.image_id = i.image_id
-#                     ;
-#                     """
-#     curr.execute(sql_query)
-#     rows = curr.fetchall()
-#     data = pd.DataFrame(rows)
-#     return data
-
-def get_filter_variables() -> int:
+def get_filter_variables(conn: connect) -> int:
     """Get the filter variables for filtering charts from streamlit multi-select"""
 
     with conn.cursor() as curr:
-        sql_query = """
-                    SELECT p.plant_id
-                    FROM s_beta.plant AS p
-                    """
-        
-        curr.execute(sql_query)
+        query = "SELECT plant_id, plant_name FROM s_beta.plant"
+        curr.execute(query, )
         rows = curr.fetchall()
-        plant_ids = [plant_id.get('plant_id') for plant_id in rows]
-        plant_id_selected = st.sidebar.selectbox("Plant Selection:",
-                                            plant_ids)
+    plant_ids = [plant_id.get('plant_id') for plant_id in rows]
+    plant_id_selected = st.sidebar.selectbox("Plant Selection:", plant_ids)
 
     return plant_id_selected
 
-def get_plant_details(conn, plant_id_selected: int):
-    """Get the relevant plant_id details to be displayed"""
-    with conn.cursor() as curr:
-        sql_query = """
-                    SELECT p.plant_id, p.plant_name, p.scientific_name, o.place_name, o.country_code, o.timezone
-                    FROM s_beta.plant AS p
-                    LEFT JOIN s_beta.origin AS o
-                        ON p.origin_id = o.origin_id
-                    WHERE p.plant_id = %d
-                    ;
-                    """
 
-        curr.execute(sql_query, plant_id_selected)
+def get_plant_details(conn: connect, plant_id_selected: int) -> tuple:
+    """Get the relevant plant_id details to be displayed"""
+
+    with conn.cursor() as curr:
+        query = """
+                SELECT p.plant_id, p.plant_name, p.scientific_name, o.place_name, o.country_code, o.timezone
+                FROM s_beta.plant AS p
+                LEFT JOIN s_beta.origin AS o
+                    ON p.origin_id = o.origin_id
+                WHERE p.plant_id = %d
+                """
+
+        curr.execute(query, plant_id_selected)
         row = curr.fetchone()
 
-    print(row)
     plant_name = row.get('plant_name')
     scientific_name = row.get('scientific_name')
-    origin = f"{row.get('country_code')}, {row.get('place_name')}, {row.get('timezone')}"
+    origin = f"{row.get('country_code', "(country_code)")}, {row.get('place_name', "(place_name)")}, {
+        row.get('timezone', "(timezone)")}"
+
     return plant_name, scientific_name, origin
+
+
+def get_total_plant_count(conn: connect) -> int:
+    """Returns total plant count."""
+
+    query = "SELECT COUNT(plant_id) AS count FROM s_beta.plant"
+
+    with conn.cursor() as curr:
+        curr.execute(query)
+        row = curr.fetchone()
+
+    return row["count"]
+
+
+def get_avg_soil_moisture(conn: connect,
+                          param: str,
+                          current: datetime = datetime.now(timezone.utc)) -> tuple[int]:
+    """Returns average soil moisture across all plants
+    and change from previous minute."""
+
+    query = f"""
+            SELECT r.recording_taken, r.{param}
+            FROM s_beta.recording AS r
+            """
+
+    with conn.cursor() as curr:
+        curr.execute(query)
+        rows = curr.fetchall()
+
+    df = pd.DataFrame(rows)
+    df['recording_taken'] = pd.to_datetime(df['recording_taken'], utc=True)
+
+    avg = df[current - df['recording_taken']
+             <= timedelta(minutes=1)][param].mean().round(2)
+    avg_prev = df[(current - df['recording_taken'] <= timedelta(minutes=2)) &
+                  (current - df['recording_taken'] > timedelta(minutes=1))][param].mean().round(2)
+
+    return avg, round(avg-avg_prev, 2)
+
 
 if __name__ == "__main__":
 
@@ -87,16 +104,36 @@ if __name__ == "__main__":
 
     conn = get_db_connection(ENV)
 
+    # DASHBOARD: PAGE SETTING
+    st.set_page_config(page_title="LMNH Plant Dashboard", page_icon="🌿", layout="wide",
+                       initial_sidebar_state="expanded", menu_items=None)
+
+    # DASHBOARD: SIDEBAR
     st.sidebar.title(":rainbow[LMNH Plant Recordings Dashboard]")
     st.sidebar.subheader("Plant recordings, no better way to see 'em")
 
-    plant_id_selected = get_filter_variables()
+    plant_id_selected = get_filter_variables(conn)
+
     st.sidebar.subheader("Plant Summary", divider="rainbow")
 
-    plant_name, scientific_name, origin = get_plant_details(conn, plant_id_selected)
+    plant_name, scientific_name, origin = get_plant_details(
+        conn, plant_id_selected)
 
     st.sidebar.write(f"Plant Name: {plant_name}")
     st.sidebar.write(f"Scientific Name: {scientific_name}")
     st.sidebar.write(f"Country, Location, Timezone: {origin}")
+
+    # DASHBOARD: MAIN
+
+    metrics = st.columns(3)
+    with metrics[0]:
+        total_plant_count = get_total_plant_count(conn)
+        st.metric("total plant count", total_plant_count)
+    with metrics[1]:
+        soil_avg, soil_delta = get_avg_soil_moisture(conn, "soil_moisture")
+        st.metric("avg soil moisture", soil_avg, soil_delta, "off")
+    with metrics[2]:
+        temp_avg, temp_delta = get_avg_soil_moisture(conn, "temperature")
+        st.metric("avg temperature", temp_avg, temp_delta, "off")
 
     conn.close()
