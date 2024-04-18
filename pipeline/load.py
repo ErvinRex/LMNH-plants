92a8e9b6ffd4309b8b6810b7301404e16248e715
Adam <trainee.adam.osullivan@sigmalabs.co.uk>
2024-04-18 11:01:37 +0100

    test workflow

--- pipeline/load.py
+++ pipeline/load.py
@@ -4,7 +4,7 @@ from pymssql import Connection, Cursor
 
 from entities import Recording, Origin, Plant, Image, Botanist
 
-# NEWTSET2
+# NEWTSET3
 
 
 def upload_data(data: list[Recording], conn: Connection) -> None:
